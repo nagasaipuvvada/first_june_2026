92f9a460a66411226b41549c0a4b7cb0882caf06
Puvvada <NagaSai.Puvvada@carrier.com>
2026-06-01 13:23:46 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.create_sheet(title="scroll")
+# Auto-adjust column widths
+for column in ws.columns:
+    max_length = 0
+    column_letter = column[0].column_letter
+    for cell in column:
+        try:
+            if len(str(cell.value)) > max_length:
+                max_length = len(str(cell.value))
+        except:
+            pass
+    adjusted_width = min(max_length + 2, 50)
+    ws.column_dimensions[column_letter].width = adjusted_width
